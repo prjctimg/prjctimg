71556e9144c1e462e1c93e482a2dde8c2bafae65
ディーン・タリサイ <arcaneqoder@gmail.com>
2025-06-28 21:21:01 +0200

    Delete .github directory

--- .github/tasks/unfollow.cjs
+++
@@ -1,6 +0,0 @@
-// unfollow everyone not following back
-var x = require("if-follow-package");
-var id = "prjctimg",
-  token = "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";
-var f = x(id, token);
-f.unfollowAllNotFollowingBack();
